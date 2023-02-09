a4fa4a2f5159ac46aea2fe23b0be29674667802a
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-02-09 18:39:33 -0300

    CRUD blog pessoal - parte 02

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +30,30 @@ public class PostagemController {
         return ResponseEntity.ok(repo.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable long id) {
+        return repo.findById(id)
+                .map(p -> ResponseEntity.ok(p))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+        return ResponseEntity.ok(repo.findAllByTituloContainingIgnoreCase(titulo));
+    }
+
+    @PostMapping
+    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repo.save(postagem));
+    }
+
+    @PutMapping
+    public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
+        return ResponseEntity.ok(repo.save(postagem));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        repo.deleteById(id);
+    }
 }
