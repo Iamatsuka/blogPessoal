1bac79cee670bbb37644fc2516c1573d8dedd5e9
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-02-09 18:15:56 -0300

    CRUD blog pessoal - parte 01

---
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,64 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String titulo;
+
+    @NotNull
+    @Size(min = 10, max = 500)
+    private String texto;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}    
+}
